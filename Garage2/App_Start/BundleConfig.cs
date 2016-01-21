2402ddaabfd40257db6d64997aeeb895eb8b2f55
Mai <eng_mai_reyad@live.com>
2016-01-21 11:10:12 +0100

    add search

--- Garage2/App_Start/BundleConfig.cs
+++ Garage2/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Garage2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
---
+++ Garage2/Controllers/FindCarController.cs
@@ -0,0 +1,25 @@
+﻿using Garage2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Garage2.Controllers
+{
+    public class FindCarController : Controller
+    {
+        // GET: FindCar
+        public ActionResult Index(string reg)
+        {
+            ViewData["reg"] = reg;
+            VehiclesDb db = new VehiclesDb();
+            var car = from c in db.vehicles  select c;
+            if (!string.IsNullOrEmpty(reg))
+            {
+                car = car.Where(c => c.RegNumber.Contains(reg));
+            }
+            return View(car);
+        }
+    }
+}
